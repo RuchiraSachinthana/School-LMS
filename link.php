3c5f06ab08dda5de64957aa8fcb24d97100c6544
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2021-11-13 21:25:24 +0530

    updated

--- link.php
+++ link.php
@@ -9,7 +9,7 @@
 					</a>
              
 							<div class="nav-collapse collapse">
-							<ul class="nav" id="footer_nav">
+							<!-- <ul class="nav" id="footer_nav">
 			<li class="divider-vertical"></li>
 		<li class="active"><a href="index.php"><i class="icon-home"></i>&nbsp;Home</a></li>
 			<li class="divider-vertical"></li>
@@ -20,10 +20,10 @@
 		<li><a href="#directories" data-toggle="modal"><i class="icon-phone"></i>&nbsp;Directories</a></li>
 				<?php include('directories.php'); ?>
 			<li class="divider-vertical"></li>
-		<
+		
 		
 	
-	</ul>
+	</ul> -->
 							</div>
                    <!--/.nav-collapse -->
                </div>
